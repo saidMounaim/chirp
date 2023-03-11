0724ebb2a6e31ab5377b75b68e647b0669018266
Said Mounaim <saidmounaim00@gmail.com>
2023-03-12 00:54:14 +0100

    Return user details & show name on navbar component

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import Login from "./Login";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
 export default async function Nav() {
+  const session = await getServerSession(authOptions);
   return (
     <nav className="flex justify-between items-center pt-4">
       <Link href="/">
         <h1 className="text-2xl font-medium">Chirp</h1>
       </Link>
-      <Login />
+      {!session?.user && <Login />}
+      {session?.user && (
+        <h4 className="text-md font-medium">{session?.user.name}</h4>
+      )}
     </nav>
   );
 }
