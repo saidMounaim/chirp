36514e8e3c5f50ecfa21ec6a15cfee56a8be7f49
Said Mounaim <saidmounaim00@gmail.com>
2023-03-20 22:54:29 +0000

    Fetch All Posts On Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 "use client";
 
 import AddPost from "./components/AddPost";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "Loading...";
+
+  console.log(data);
+
   return (
     <main>
       <AddPost />
