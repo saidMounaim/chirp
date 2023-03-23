77fe561b7e1e64d141b216a0a855ac16195724b1
Said Mounaim <saidmounaim00@gmail.com>
2023-03-23 19:41:54 +0000

    Add Dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <main>
+      <h3 className="text-lg font-medium">
+        Welcome Back {session?.user?.name}
+      </h3>
+    </main>
+  );
+}
