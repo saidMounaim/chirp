5801f47714859e7f44ae2cc54554a03a256f1892
Said Mounaim <saidmounaim00@gmail.com>
2023-03-25 05:29:07 +0000

    Add Delete post functionality

--- app/components/EditPost.tsx
+++ app/components/EditPost.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Toggle from "./Toggle";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 interface IPost {
   post: {
+    id: string;
     avatar: string;
     title: string;
     user: {
@@ -21,6 +25,25 @@ interface IPost {
 }
 
 const EditPost = ({ post }: IPost) => {
+  const [toggle, setToggle] = useState(false);
+
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id }),
+    {
+      onError: (error: any) => {
+        toast.error(error?.response?.data.message);
+      },
+      onSuccess: (data) => {
+        toast.success("Post has been deleted");
+      },
+    }
+  );
+
+  const deletePost = () => {
+    mutate(post?.id);
+  };
+
   return (
     <>
       <div className="bg-white my-8 p-8 rounded-lg">
@@ -39,12 +62,15 @@ const EditPost = ({ post }: IPost) => {
         </div>
         <div className="mt-3 flex items-center justify-between">
           <p>{post?.comments?.length} Comments</p>
-          <button className="p-3 flex items-center justify-center font-medium text-md text-red-500">
+          <button
+            onClick={() => setToggle(true)}
+            className="p-3 flex items-center justify-center font-medium text-md text-red-500"
+          >
             Delete
           </button>
         </div>
       </div>
-      <Toggle />
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
     </>
   );
 };
--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -1,12 +1,26 @@
-const Toggle = () => {
+type ToggleProps = {
+  deletePost: () => void;
+  setToggle: (toggle: boolean) => void;
+};
+
+const Toggle = ({ deletePost, setToggle }: ToggleProps) => {
   return (
-    <div className="fixed bg-black/30 w-full h-full top-0 left-0 z-20">
+    <div
+      onClick={(e) => {
+        e.stopPropagation();
+        setToggle(false);
+      }}
+      className="fixed bg-black/50 w-full h-full top-0 left-0 z-20"
+    >
       <div className="absolute bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-12 rounded-lg flex flex-col gap-6">
         <h2 className="text-xl">Are you sure you want to delete this post?</h2>
         <h3 className="text-red-600 text-sm">
           Pressing the delete button will permenantly delete your post
         </h3>
-        <button className="bg-red-600 text-sm text-white py-2 px-4 rounded-md">
+        <button
+          onClick={deletePost}
+          className="bg-red-600 text-sm text-white py-2 px-4 rounded-md"
+        >
           Delete Post
         </button>
       </div>
